c8b13e9bff3d8609a5e7589a3315c87a9d2368a1
nehagupta8864-create <neha.gupta8864@gmail.com>
2025-12-08 23:08:41 +0530

    Comment out rent calculator code
    Commented out the entire rent calculator code.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# # Rent Calculator
 
+# print("Let's print Rent Calculator now: ")
+
+# rent= int(input("Enter the amount of rent here: "))
+# food= int(input("Enter the amount of food ordered: "))
+# electricity_spend= int(input("Enter the total of electricity spend: "))
+# charge_per_unit= int(input("Enter the charge per unit: "))
+# persons= int(input("Enter the number of persons living in the room/flat: "))
+
+# total_bill= electricity_spend*charge_per_unit
+# total_amount_spent= rent+food+total_bill
+# rent_amount_per_person= total_amount_spent//persons
+
+# print(f"Each person will pay: Rs.{rent_amount_per_person}.")
